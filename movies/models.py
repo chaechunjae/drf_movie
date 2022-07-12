35e9cf575dbe66245d00076b3dbd9aaddab1e6b3
MyengJu-Chae <myeongju@dable.io>
2022-07-13 00:31:42 +0900

    set imagefield, default image

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
@@ -11,3 +12,4 @@ class Moviedata(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     typ = models.CharField(max_length=200, default='action')
+    image = models.ImageField(upload_to='Images/', default="Images/None/No_img.jpg")
